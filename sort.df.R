39954397877bd0e8789ae2b893428be5b30c87f9
Jacobo Perez <jacopeig@gmail.com>
2017-02-24 09:47:01 +0000

    Add files via upload

---
+++ sort.df.R
@@ -0,0 +1,41 @@
+sort.df<-function(df,word,columns){
+	if(class(df)!="data.frame") stop("No data frame as argument")
+	if(class(word)!="character") stop("No character as argument")
+	if(class(columns)!="numeric") stop("Not valid number of columns")
+	if(max(columns)>ncol(df) | min(columns)<0) stop("out of range")	
+
+
+	A<-matrix(0,nrow=nrow(df),ncol=length(columns))
+	
+	for (j in 1:length(columns)){
+		for(i in 1:nrow(df)){
+			if(is.na(pmatch(word,df[i,columns[j]]))){
+				A[i,j]<-0
+			}else{
+				A[i,j]<-1
+			}
+		}
+	}
+
+	dff<-as.data.frame(A)
+	
+	e<-0
+	for(i in 1:nrow(dff)){
+		e[i]<-sum(dff[i,])
+		if (e[i]>1) stop("Two repeated words in several columns")
+	}
+	
+	for (i in 1:ncol(dff)){
+		df[,columns[i]]<-as.character(df[,columns[i]])
+	}
+	a<-0
+	for (j in 1:length(columns)){
+		for (i in 1:nrow(df)){
+			if(dff[i,j]==1){
+				a<-df[i,columns[1]]
+				df[i,columns[1]]<-df[i,columns[j]]
+				df[i,columns[j]]<-a
+			}
+		}
+	}
+}
